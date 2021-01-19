ee1f997580ce0e34d02d30eb340891a2c11131c6
Felix Bade <flix.bade@gmail.com>
2021-01-19 18:07:08 +0200

    Change React class to a function

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-export class App extends Component {
-    render() {
-        return (
-            <h1>Hello there!</h1>
-        )
-    }
+export default function App() {
+    return (
+        <h1>Hello there!</h1>
+    )
 }
 
 ReactDOM.render(
