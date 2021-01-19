e7ca03ee9bffbb22270a7ae96e15dec95fa6daae
Felix Bade <flix.bade@gmail.com>
2021-01-19 15:59:37 +0200

    Add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'parser': 'babel-eslint',
+    'plugins': [
+        'react',
+        'html'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+    },
+    'settings': {
+        'react': {
+            'pragma': 'React',
+            'version': 'detect',
+        },
+    }
+}
