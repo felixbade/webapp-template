1031d3c717528d2bb78e3065216a59e90dd8c3de
Felix Bade <flix.bade@gmail.com>
2021-01-19 15:59:16 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle-[contenthash].js',
+        publicPath: ''
+    },
+    devServer: {
+        port: 8080,
+        historyApiFallback: true
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
